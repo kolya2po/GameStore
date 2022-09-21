506a8904e866243eed1b357bc901a36693a8f97a
Mykola Stepanenko <ironfack78@gmail.com>
2022-09-21 20:07:42 +0300

    added comments service and also comments repository to UoW

---
+++ GameStore.BLL/Interfaces/ICommentsService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using GameStore.BLL.Models;
+
+namespace GameStore.BLL.Interfaces
+{
+    public interface ICommentsService
+    {
+        Task<CommentModel> ReplyToCommentAsync(CommentModel parentComment,
+            CommentModel reply);
+        Task<CommentModel> CreateCommentForGameAsync(GameModel gameModel, CommentModel commentModel);
+        Task UpdateAsync(CommentModel model);
+        Task DeleteAsync(int modelId);
+    }
+}
---
+++ GameStore.BLL/Services/CommentsService.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using GameStore.BLL.Interfaces;
+using GameStore.BLL.Models;
+using GameStore.DAL.Entities;
+using GameStore.DAL.Interfaces;
+
+namespace GameStore.BLL.Services
+{
+    public class CommentsService : BaseService, ICommentsService
+    {
+        public CommentsService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) {  }
+
+        // Should I return created comment from db? 
+        // Because I need to have comment with id on the client 
+        // to have possibility to make reply.
+        public async Task<CommentModel> ReplyToCommentAsync(CommentModel parentComment, CommentModel reply)
+        {
+            var comment = Mapper.Map<Comment>(reply);
+            comment.ParentCommentId = parentComment.Id;
+
+            await UnitOfWork.CommentsRepository.CreateAsync(comment);
+            await UnitOfWork.SaveChangesAsync();
+
+            return reply;
+        }
+
+        // Should I return created comment from db? 
+        // Because I need to have comment with id on the client 
+        // to have possibility to make reply.
+        public async Task<CommentModel> CreateCommentForGameAsync(GameModel gameModel, CommentModel commentModel)
+        {
+            var comment = Mapper.Map<Comment>(commentModel);
+            comment.GameId = gameModel.Id;
+
+            await UnitOfWork.CommentsRepository.CreateAsync(comment);
+            await UnitOfWork.SaveChangesAsync();
+
+            return commentModel;
+        }
+
+        public async Task UpdateAsync(CommentModel model)
+        {
+            var comment = Mapper.Map<Comment>(model);
+
+            UnitOfWork.CommentsRepository.Update(comment);
+            await UnitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int modelId)
+        {
+            await UnitOfWork.CommentsRepository.DeleteByIdAsync(modelId);
+            await UnitOfWork.SaveChangesAsync();
+        }
+    }
+}
