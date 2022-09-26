6234a328b1d7b71d1ef250cfdcb3b6bc8fe10308
Mykola Stepanenko <ironfack78@gmail.com>
2022-09-26 14:08:27 +0300

    added cart entity and model

---
+++ GameStore.DAL/Entities/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GameStore.DAL.Entities
+{
+    public class Cart : BaseEntity
+    {
+        public IEnumerable<CartItem> CartItems { get; set; }
+        public decimal TotalPrice => CartItems.Sum(c => c.Quantity * c.Game.Price);
+        public int UserId { get; set; }
+    }
+}
---
+++ GameStore.DAL/Entities/CartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.DAL.Entities
+{
+    public class CartItem : BaseEntity
+    {
+        public Game Game { get; set; }
+        public int Quantity { get; set; }
+    }
+}
