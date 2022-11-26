c82ab208219eea7282f3d4e1095b9987342dc5c6
Mykola Stepanenko <ironfack78@gmail.com>
2022-11-26 15:03:30 +0200

    add test for games repository

---
+++ GameStore.Tests/DataTests/GamesRepositoryTests.cs
@@ -0,0 +1,133 @@
+﻿using FluentAssertions;
+using GameStore.DAL;
+using GameStore.DAL.Entities;
+using GameStore.DAL.Repositories;
+using NUnit.Framework;
+
+namespace GameStore.Tests.DataTests;
+
+[TestFixture]
+public class GamesRepositoryTests
+{
+    private GameStoreDbContext _context = null!;
+    private GamesRepository _repository = null!;
+
+    [SetUp]
+    public void InitializeDbContextAndGamesRepository()
+    {
+        _context = new GameStoreDbContext(UnitTestHelper.GetDbContextOptions());
+        _repository = new GamesRepository(_context);
+    }
+
+    [TearDown]
+    public async Task CleanupDbContext()
+    {
+        await _context.Database.EnsureDeletedAsync();
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    public async Task GetByIdAsync_ShouldReturnGameById_WhenGameExists(int id)
+    {
+        // Arrange
+        var expected = UnitTestHelper.GetTestGames().First(g => g.Id == id);
+
+        // Act
+        var actual = await _repository.GetByIdAsync(id);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public async Task GetAllAsync_ShouldReturnAllGamesFromDb()
+    {
+        // Arrange
+        var expected = UnitTestHelper.GetTestGames();
+
+        // Act
+        var actual = await _repository.GetAllAsync();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public async Task CreateAsync_ShouldAddNewGameToDb()
+    {
+        // Arrange
+        var initialCount = UnitTestHelper.GetTestGames().Count();
+
+        var newGame = new Game
+        {
+            Id = 4,
+            Name = "game 4",
+            AuthorId = 1
+        };
+
+        // Act
+        await _repository.CreateAsync(newGame);
+        await _context.SaveChangesAsync();
+
+        // Assert
+        _context.Games.Count().Should().Be(initialCount + 1);
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    public async Task DeleteByIdAsync_ShouldDeleteGameByIdFromDb(int id)
+    {
+        // Arrange
+        var initialCount = UnitTestHelper.GetTestGames().Count();
+
+        // Act
+        await _repository.DeleteByIdAsync(id);
+        await _context.SaveChangesAsync();
+
+        // Assert
+        _context.Games.Count().Should().Be(initialCount - 1);
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    public async Task Delete_ShouldDeleteGameFromDb(int id)
+    {
+        // Arrange
+        var initialCount = UnitTestHelper.GetTestGames().Count();
+        var gameToDelete = UnitTestHelper.GetTestGames().First(c => c.Id == id);
+
+        // Act
+        _repository.Delete(gameToDelete);
+        await _context.SaveChangesAsync();
+
+        // Assert
+        _context.Games.Count().Should().Be(initialCount - 1);
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    public async Task Update_ShouldUpdateGamesProperties(int id)
+    {
+        // Arrange
+        var oldGame = UnitTestHelper.GetTestGames().First(c => c.Id == id);
+
+        var newGame = new Game
+        {
+            Id = id,
+            Name = $"new game {id}",
+            Description = $"desc {id}",
+            Price = 230.33m,
+            AuthorId = 1
+        };
+
+        // Act
+        _repository.Update(newGame);
+        await _context.SaveChangesAsync();
+
+        // Assert
+        newGame.Should().NotBeEquivalentTo(oldGame);
+    }
+
+
+}
--- GameStore.Tests/UnitTestHelper.cs
+++ GameStore.Tests/UnitTestHelper.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
 using GameStore.BLL;
+using GameStore.DAL;
+using GameStore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Tests
 {
-    public class UnitTestHelper
+    public static class UnitTestHelper
     {
         public static IMapper CreateMapperFromProfile()
         {
@@ -12,5 +15,55 @@ namespace GameStore.Tests
 
             return new Mapper(config);
         }
+
+        public static DbContextOptions<GameStoreDbContext> GetDbContextOptions()
+        {
+            var options = new DbContextOptionsBuilder<GameStoreDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .EnableSensitiveDataLogging()
+                .Options;
+
+            using var context = new GameStoreDbContext(options);
+
+            SeedData(context);
+
+            return options;
+        }
+
+        private static void SeedData(GameStoreDbContext dbContext)
+        {
+            dbContext.Games.AddRange(GetTestGames());
+
+            dbContext.SaveChanges();
+        }
+
+        public static IEnumerable<Game> GetTestGames() =>
+            new[]
+            {
+                new Game
+                {
+                    Id = 1,
+                    Name = "game 1",
+                    Description = "desc 1",
+                    Price = 230.33m,
+                    AuthorId = 1
+                },
+                new Game
+                {
+                    Id = 2,
+                    Name = "game 2",
+                    Description = "desc 2",
+                    Price = 20.31m,
+                    AuthorId = 2
+                },
+                new Game
+                {
+                    Id = 3,
+                    Name = "game 3",
+                    Description = "desc 3",
+                    Price = 10.13m,
+                    AuthorId = 3
+                }
+            };
     }
 }
