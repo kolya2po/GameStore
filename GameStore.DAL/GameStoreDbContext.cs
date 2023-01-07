1eb218c56e04b273d46672777df79f2250d51019
Mykola Stepanenko <ironfack78@gmail.com>
2023-01-07 13:58:26 +0200

    removed redundant code from db context

--- GameStore.DAL/GameStoreDbContext.cs
+++ GameStore.DAL/GameStoreDbContext.cs
@@ -8,16 +8,7 @@ namespace GameStore.DAL
 {
     public class GameStoreDbContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
-        public GameStoreDbContext()
-        {
-
-        }
         public GameStoreDbContext(DbContextOptions<GameStoreDbContext> options) : base(options) { }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    base.OnConfiguring(optionsBuilder);
-        //    optionsBuilder.UseSqlServer("Server=KOLYAPC;Database=gamestoredb;Trusted_Connection=true;");
-        //}
 
         public DbSet<Game> Games { get; set; }
         public DbSet<GameGenre> GameGenres { get; set; }
